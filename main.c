48f3043120895bed7c3ec847f96441b8498f10d4
dominickdemilio <132515000+dominickdemilio@users.noreply.github.com>
2023-05-03 23:30:57 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 //
 //  File: main.c
 //  ECE Final Project: A Game of Dos
@@ -21,18 +22,18 @@
 bool validPlay(card centerCard, int input1, int input2, hand* currentHand) {
     card card1, card2;
     hand tempHand = *currentHand;
-    
-    for (int i = 1 ; i < currentHand->numCards + 1; i++) {
+
+    for (int i = 1; i < currentHand->numCards + 1; i++) {
         if (i == input1)
             card1 = *tempHand.playerHand;
         if (i == input2)
             card2 = *tempHand.playerHand;
         tempHand.playerHand = tempHand.playerHand->pt;
     }
-    
+
     // 1 input
     if (input2 < 0) {
-        if(card1.value == centerCard.value || strcmp(card1.action, "AnyNumber") == 1 || strcmp(centerCard.action, "AnyNumber") == 1) {
+        if (card1.value == centerCard.value || strcmp(card1.action, "AnyNumber") == 1 || strcmp(centerCard.action, "AnyNumber") == 1) {
             deleteNode(&currentHand->playerHand, input1);
             return true;
         }
@@ -42,10 +43,10 @@ bool validPlay(card centerCard, int input1, int input2, hand* currentHand) {
             return false;
         }
     }
-    
+
     // 2 inputs
     else {
-        if(card1.value + card2.value == centerCard.value || strcmp(card1.action, "AnyNumber") == 1 || strcmp(card2.action, "AnyNumber") == 1) {
+        if (card1.value + card2.value == centerCard.value || strcmp(card1.action, "AnyNumber") == 1 || strcmp(card2.action, "AnyNumber") == 1) {
             deleteNode(&currentHand->playerHand, input1);
             deleteNode(&currentHand->playerHand, input2);
             return true;
@@ -56,7 +57,7 @@ bool validPlay(card centerCard, int input1, int input2, hand* currentHand) {
             return false;
         }
     }
-    
+
     // THIS FUNCTION NEEDS TO BE POLISHED
 }
 
@@ -64,7 +65,7 @@ void playerTurn(hand* currentHand, hand* centerRow, card* deck) {
     int userInput;
     int userInput2;
     card activeCenterCard;
-    
+
     //Prints discard card and player's hand
     printf("Player %d's hand: ", currentHand->playerNum);
     print(currentHand->playerHand);
@@ -72,13 +73,13 @@ void playerTurn(hand* currentHand, hand* centerRow, card* deck) {
     print(centerRow->playerHand);
 
     //Player decides which card to play
-    for (int i = 0 ; i < centerRow->numCards ; i++) {
+    for (int i = 0; i < centerRow->numCards; i++) {
         activeCenterCard = *centerRow->playerHand;
         printf("How many cards do you want to play on %d %s: ", activeCenterCard.value, activeCenterCard.color);
         scanf("%d", &userInput);
-        
+
         // chooses what card
-        if(userInput == 1) {
+        if (userInput == 1) {
             printf("Select a card from 1-%d: ", currentHand->numCards);
             scanf("%d", &userInput);
             validPlay(*centerRow->playerHand, userInput, -1, currentHand);
@@ -88,22 +89,22 @@ void playerTurn(hand* currentHand, hand* centerRow, card* deck) {
             scanf("%d, %d", &userInput, &userInput2);
             validPlay(*centerRow->playerHand, userInput, userInput2, currentHand);
         }
-        
-        
+
+
         // NEEDS LOGIC HERE
-        
+
         activeCenterCard = *activeCenterCard.pt;
     }
-    
+
     // AND LOGIC HERE
-    
-    
+
+
     // THIS STUFF IS FOR WHEN THE USER DOES NOT PLAY A CARD, AND HAS TO DRAW
     drawCard(currentHand, &deck);
     print(currentHand->playerHand);
     printf("You drew a card. Press 1 to make a match or 0 to discard: ");
     scanf("%d", &userInput);
-    
+
 }
 
 //THE MAIN FUNCTION
@@ -111,40 +112,40 @@ int main(void) {
     bool winner = false;
     int playerToAct = 0;
     hand centerRow;
-    
+
     centerRow.playerHand = NULL;
     centerRow.playerHand = 0;
-    
+
     //set up players and the deck
     int numPlayers = init_players();
     card* deck = init_deck();
-        
+
     //initialize hands
     hand PlayersHands[numPlayers];
-    for(int i = 0 ; i < numPlayers ; i++) {
+    for (int i = 0; i < numPlayers; i++) {
         PlayersHands[i].points = 0;
         PlayersHands[i].playerNum = i + 1;
         PlayersHands[i].playerHand = NULL;
         PlayersHands[i].numCards = 0;
     }
-    for(int i = 0 ; i < HANDSIZE; i++)
-        for(int j = 0 ; j < numPlayers ; j++)
+    for (int i = 0; i < HANDSIZE; i++)
+        for (int j = 0; j < numPlayers; j++)
             drawCard(&PlayersHands[j], &deck); //function "deals" cards to each player in alternating order by taking top card off the deck and adding it to their hand
-    
+
     //Turns over first 2 cards
-    for(int i = 0 ; i < 2 ; i++)
+    for (int i = 0; i < 2; i++)
         drawCard(&centerRow, &deck);
-    
+
     //Start of Gameplay
-    while(!winner) {
+    while (!winner) {
         playerTurn(&PlayersHands[playerToAct], &centerRow, deck);
-        
+
         // NEED MORE GAMEPLAY LOGIC HERE
-        
+
     }
 
     //frees all dynamically allocated memory and ends function
-    for(int i = 0 ; i < numPlayers ; i++)
+    for (int i = 0; i < numPlayers; i++)
         free(PlayersHands[i].playerHand);
     free(deck);
     return 0;
